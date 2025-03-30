0e4ea43ff2c00acdcdb88b7c808ee4f8a607baaa
MaksimNikiforov <145759459+fblfca@users.noreply.github.com>
2025-03-30 12:06:02 +0300

    tcp_client num5

--- tcp_client.py
+++ tcp_client.py
@@ -1,32 +1,38 @@
 import socket
+import logging
 
 def get_port_and_host():
     host = input("Введите имя хоста (по умолчанию 'localhost'): ") or 'localhost'
     port = int(input("Введите номер порта (по умолчанию 65432): ") or 65432)
     return host, port
 
+def setup_logger():
+    logging.basicConfig(filename='server.log', level=logging.INFO,
+                        format='%(asctime)s - %(message)s')
+
 def start_server(host='localhost', port=65432):
+    setup_logger()
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((host, port))
         s.listen()
-        print("Сервер запущен и слушает порт", port)
+        logging.info(f"Сервер запущен и слушает порт {port}")
 
         while True:
             conn, addr = s.accept()
             with conn:
-                print("Подключение от", addr)
+                logging.info(f"Подключение от {addr}")
                 while True:
                     data = conn.recv(1024)
                     if not data:
                         break
                     message = data.decode()
-                    print("Получены данные от клиента:", message)
+                    logging.info(f"Получены данные от клиента: {message}")
                     if message.lower() == "exit":
-                        print("Клиент запросил разрыв соединения:", addr)
+                        logging.info(f"Клиент запросил разрыв соединения: {addr}")
                         break
                     conn.sendall(data)
-                    print("Данные отправлены клиенту:", message)
-                print("Клиент отключен:", addr)
+                    logging.info(f"Данные отправлены клиенту: {message}")
+                logging.info(f"Клиент отключен: {addr}")
 
 if __name__ == "__main__":
     host, port = get_port_and_host()
